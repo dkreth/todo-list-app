04074e97b7ff156db83a365c4d33082f6a12b622
Dylan Kreth <dylankreth@gmail.com>
2019-03-11 09:18:17 -0500

    add key prop to TodoItem as we map through. Add propTypes to Todos

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
     return this.props.todoList.map((todo) => (
-      <h3>{ todo.title } </h3>
+      <TodoItem key = {todo.id} todo = {todo}/>
     ));
   }
 }
 
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+}
 export default Todos;
