c03ce92728db9ef2d24ffec93718f3e6f9dccfde
Dylan Kreth <dylankreth@gmail.com>
2019-03-11 15:22:36 -0500

    make AddTodo component and add to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css'
 import Todos from './components/Todos';
 import Header from './components/layout/Header'
+import AddTodo from './components/AddTodo'
 
 class App extends Component {
   state = {
@@ -44,8 +45,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-      <Todos todoList = {this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todoList = {this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
+        </div>
       </div>
     );
   }
