d6ae0ae3f6efdc395af88b85acd38a8b404b9c2a
Dylan Kreth <dylankreth@gmail.com>
2019-03-11 15:54:28 -0500

    implement functionality of AddTodo

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css'
 import Todos from './components/Todos';
 import Header from './components/layout/Header'
 import AddTodo from './components/AddTodo'
+import uuid from 'uuid'
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with Jules',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with Mandie',
         completed: false
       },
@@ -42,12 +43,21 @@ class App extends Component {
     })
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo]})
+  }
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo}/>
           <Todos todoList = {this.state.todos} markComplete = {this.markComplete} delTodo={this.delTodo}/>
         </div>
       </div>
