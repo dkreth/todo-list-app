b03b262af5f1cb9d5e166319bb1b8cfa02ccb3d0
Dylan Kreth <dylankreth@gmail.com>
2019-03-11 09:07:17 -0500

    create TodoItem class

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is the body of the TodoItem </p>
+      </div>
+    )
+  }
+}
+
+export default TodoItem
